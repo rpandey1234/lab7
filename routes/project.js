4751ce98e3736a7f0a92e93548d0ed42989261af
Rahul Pandey <rpandey1234@gmail.com>
2014-02-19 15:30:52 -0800

    db interaction

--- routes/project.js
+++ routes/project.js
@@ -2,9 +2,12 @@ var models = require('../models');
 
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
-
+  console.log('finding project');
   // query for the specific project and
   // call the following callback
+  models.Project
+    .find( {"_id": projectID} )
+    .exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -16,13 +19,30 @@ exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
 
+  var newPost = new models.Project(form_data);
+  newPost.save(afterSaving);
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
+  
+  function afterSaving(err) { // this is a callback
+    if(err) {console.log(err); res.send(500); }
+    console.log('successfully saved');
+    res.send();
+  }
 }
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
 
   // find the project and remove it
+  models.Project
+    .find( {"_id": projectID} )
+    .remove()
+    .exec(afterDelete);
+
   // YOU MUST send an OK response w/ res.send();
+  function afterDelete(err, projects) {
+    if(err) console.log(err);
+    res.send();
+  }
 }
